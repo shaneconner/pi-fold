1f089f8067dcc60e3b6d198311802b951b0dd1c4
Shane Conner <shanepatrickconner@gmail.com>
2026-08-04 22:11:12 -0500

    Add release linting and CI

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,ts}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "no-control-regex": "off",
+      "no-empty": ["error", { allowEmptyCatch: true }],
+      "no-unused-vars": ["error", { args: "none" }],
+      "no-useless-assignment": "off",
+      "@stylistic/eol-last": ["error", "always"],
+      "@stylistic/no-trailing-spaces": "error",
+      "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+      "@stylistic/semi": ["error", "always"],
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "off",
+    },
+  },
+];
